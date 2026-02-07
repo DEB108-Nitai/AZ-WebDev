d1838beb214f43d0b5e2b9cd9ee9387083e3b13d
DEB108-Nitai <devranjanpramanick@gmail.com>
2026-02-07 08:10:21 +0000

    basics

---
+++ React/JS REFRESHER/Basics.js
@@ -0,0 +1,12 @@
+console.log("Hare Krishna")
+
+// let , const 
+const userMassage = " Hare Krsna "
+console.log(userMassage) 
+let userId = 12
+console.log(userId)
+console.log("Hare" + "krsna")
+// operators 
+console.log(10/5) // + , - , * , % , / everything works 
+if("10" == 10) console.log(userId) // == checks only value
+if( 10 === 10) console.log(userId) // === checks the value and data type both 
